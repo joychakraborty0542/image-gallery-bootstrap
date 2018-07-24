844d0951234f7592b5655ba91e3c82769dee6b9f
joychakraborty0542 <40673527+joychakraborty0542@users.noreply.github.com>
2018-07-24 17:11:38 +0530

    first java script file of age calculator

---
+++ New folder/intro.js
@@ -0,0 +1,7 @@
+var f_1=prompt("what is your first name?");
+var l_1=prompt("what is ypur last name?");
+alert("nice to meet you " +f_1 + "" + l_1);
+console.log("your fullname is " +f_1 + "" + l_1);
+var age=prompt("wha is your age?");
+console.log("your age is " +age);
+alert("yourage is roughly " +age*365+"days");
